f6603399d3e0cb7a2b80396999dbdf3e918cde27
AstroAnmol <anmolsikka09@gmail.com>
2021-10-09 01:54:55 -0400

    minor comments added

--- Poly_Fitting/checking_fitting.py
+++ Poly_Fitting/checking_fitting.py
@@ -1,3 +1,5 @@
+# Parallel Case
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
--- Poly_Fitting/checking_fitting_2.py
+++ Poly_Fitting/checking_fitting_2.py
@@ -1,3 +1,5 @@
+#Perpendicular Case
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
