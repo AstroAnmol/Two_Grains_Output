e1db040865d573e6677280d6922c70a42dcfaaa0
AerospaceNerd <anmolsikka09@gmail.com>
2022-02-04 15:43:11 -0500

    generating fitting plots for each susceptibility

--- Poly_Fitting/scipy_curvefit.py
+++ Poly_Fitting/scipy_curvefit.py
@@ -1,73 +1,137 @@
+from unittest import case
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 import pandas as pd
+import sys
 
 ## Polynomial fitting for mag data
 
-
-# model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
-def func(c, p1, p2, p3):
-    mu0=4.0*np.pi*1e-7
-    susc_eff=3*susc/(3+susc)
-    # F_DM=-8*np.pi*susc_eff**2/(3*c**4) # (parallel case)
-    F_DM=4*np.pi*susc_eff**2/(3*c**4) # (perpendicular case)
-    return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
-
-# #model function F = mu0*(p0/r^ + p1/r^5 + p2/r^6 + p3/r^7)
-# def func(c, p0, p1, p2, p3):
-#     mu0=4.0*np.pi*1e-7
-#     return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
+case_selection=1 ## 0 for parallel and 1 for perpendicular
 
 #import the data
-data_H1_a1_th0=pd.read_csv('Data/data_H1_a1_th0.csv')
-data_H1_a1_th90=pd.read_csv('Data/data_H1_a1_th90.csv')
+data_H1_a1_th0=pd.read_csv('Data/csv_files/data_H1_a1_th0.csv')
+data_H1_a1_th90=pd.read_csv('Data/csv_files/data_H1_a1_th90.csv')
 
-data=data_H1_a1_th90
+# data=data_H1_a1_th90
 # data=data_H1_a1_th0
-
-susc_data=np.array([0.25,	0.5,	0.75,	1,	1.5,	2,	2.5,	3,	3.5,	4,	4.5,	5,	5.5,	6,	6.5,	7,	7.5,	8])
-c_data=data['c']    
-
-
-for ind in range(18):
-    susc_strings=np.array(['0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0','7.5','8.0'])
-    susc=susc_data[ind]
-    fz_data=data[susc_strings[ind]]
-
-    #Fit for the parameters of the function func:
-    popt, pcov = curve_fit(func, c_data, fz_data)
+if case_selection==0:
+    file_path = 'Results_txt/fitting_results_th00.txt'
+    sys.stdout = open(file_path, "w")
     
-    #One standard deviation for parameters
-    perr = np.sqrt(np.diag(pcov))
-    print('Susceptibilty:'+susc_strings[ind])
-    print('Optimised parameters',popt)
-    print('Standard Deviation of Parameters:',perr)
+    data=data_H1_a1_th0
+    print("####################################################")
+    print("Theta=0 deg case (Parallel)")
+    print("####################################################")
+    
+elif case_selection==1:
+    file_path = 'Results_txt/fitting_results_th90.txt'
+    sys.stdout = open(file_path, "w")
+    
+    data=data_H1_a1_th90
+    print("####################################################")
+    print("Theta=90 deg case (Perpendicular)")
+    print("####################################################")
 
-    # plt.plot(c_data, func(c_data, popt[0]+perr[0],popt[1]-perr[1],popt[2]+perr[2],popt[3]-perr[3]), 'r--')
-    # plt.plot(c_data, func(c_data, popt[0]-perr[0],popt[1]+perr[1],popt[2]-perr[2],popt[3]+perr[3]), 'r--')
+c_data=data['c']
 
-    #Calculate Least Sqaures error
-    fz_cal=func(c_data, *popt)
-    residuals=fz_data-fz_cal
-    l_sq_err=np.sum(residuals**2)
-    squaresum = np.sum((fz_data-np.mean(fz_data))**2)
-    R2 = 1 - (l_sq_err/squaresum)
-    print('Least squares error',l_sq_err)
-    print('R^2 value',R2)
-    print()
+if case_selection==0:
+    susc_data=np.array([0.1, 0.15, 0.2, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, \
+        7.5, 8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 65.0, 70.0, 75.0, 80.0, 85.0, 90.0, 95.0, 100.0])
+    tot_ind=susc_data.size
+elif case_selection==1:
+    susc_data=np.array([0.1, 0.15, 0.2, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, \
+        8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0, 15.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0])
+    tot_ind=susc_data.size
 
-    # plot
-    plt.figure()
-    plt.plot(c_data, fz_data, 'b+', label='data')
-    plt.plot(c_data, func(c_data, *popt), 'r-',\
-        # label='model fit: p0=%5.3f, p1=%5.3f, p2=%5.3f, p3=%5.3f' % tuple(popt))
-        label='model fit: p1=%5.3f, p2=%5.3f, p3=%5.3f' % tuple(popt))
-    plt.xlabel('c')
-    plt.ylabel('fz (N)')
-    plt.title(r"a=1 m, H0=1 A/m, $\theta$=90, $\chi=$"+susc_strings[ind])
-    plt.grid('on')
-    plt.legend()
-    susc_strings=np.array(['0,25','0,50','0,75','1,0','1,5','2,0','2,5','3,0','3,5','4,0','4,5','5,0','5,5','6,0','6,5','7,0','7,5','8,0'])
-    plt.savefig('Plots/Fitting/Perpendicular_Case/a_1_H0_1_th_90_chi_'+susc_strings[ind]+'.png')
-    # plt.savefig('Plots/Fitting/Parallel_Case/a_1_H0_1_th_0_chi_'+susc_strings[ind]+'.png')
+for i in range(2):
+    func_selection=i
+    if func_selection==0:
+        print("Fitting for FDM based function")
+        print()
+        print()
+        # model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
+        def func(c, p1, p2, p3):
+            mu0=4.0*np.pi*1e-7
+            susc_eff=3*susc/(3+susc)
+            # F_DM=-8*np.pi*susc_eff**2/(3*c**4) # (parallel case)
+            F_DM=4*np.pi*susc_eff**2/(3*c**4) # (perpendicular case)
+            return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
+    
+    elif func_selection==1:
+        print("Fitting for varied based function")
+        print()
+        print()
+        #model function F = mu0*(p0/r^ + p1/r^5 + p2/r^6 + p3/r^7)
+        def func(c, p0, p1, p2, p3):
+            mu0=4.0*np.pi*1e-7
+            return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
+
+    for ind in range(tot_ind):
+        if case_selection==0:
+            susc_strings=np.array(['0.1','0.15','0.2','0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0',\
+                '7.5','8.0','8.5','9.0','9.5','10.0','10.5','11.0','11.5','12.0','15.0','20.0','25.0','30.0','35.0','40.0','45.0','50.0','55.0','60.0','65.0','70.0','75.0','80.0','85.0','90.0','95.0','100.0'])
+        elif case_selection==1:
+            susc_strings=np.array(['0.1','0.15','0.2','0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0','7.5','8.0',\
+                '8.5','9.0','9.5','10.0','10.5','11.0','11.5','12.0','15.0','20.0','30.0','40.0','50.0','60.0','70.0','80.0','90.0','100.0'])
+        susc=susc_data[ind]
+        fz_data=data[susc_strings[ind]]
+
+        #Fit for the parameters of the function func:
+        popt, pcov = curve_fit(func, c_data, fz_data)
+
+        #One standard deviation for parameters
+        perr = np.sqrt(np.diag(pcov))
+        print('Susceptibilty:'+susc_strings[ind])
+        print('Optimised parameters',popt)
+        print('Standard Deviation of Parameters:',perr)
+
+        #Calculate Least Sqaures error
+        fz_cal=func(c_data, *popt)
+        print("Calculated force")
+        print(np.array(fz_cal))
+        residuals=fz_data-fz_cal
+        l_sq_err=np.sum(residuals**2)
+        squaresum = np.sum((fz_data-np.mean(fz_data))**2)
+        R2 = 1 - (l_sq_err/squaresum)
+        print('Least squares error',np.sqrt(l_sq_err))
+        print('R^2 value',R2)
+        print()
+
+        # plot
+        plt.figure()
+        plt.plot(c_data, fz_data, 'b+', label='data')
+        if func_selection==0:
+            plt.plot(c_data, func(c_data, *popt), 'r-', label='FDM based model fit')
+        elif func_selection==1:
+            plt.plot(c_data, func(c_data, *popt), 'r-', label='Varied model fit')
+        
+        # if func_selection==0:
+        #     plt.plot(c_data, func(c_data, popt[0]+perr[0],popt[1]-perr[1],popt[2]+perr[2]), 'r--')
+        #     plt.plot(c_data, func(c_data, popt[0]-perr[0],popt[1]+perr[1],popt[2]-perr[2]), 'r--')
+        # elif func_selection==1:
+        #     plt.plot(c_data, func(c_data, popt[0]+perr[0],popt[1]-perr[1],popt[2]+perr[2],popt[3]-perr[3]), 'r--')
+        #     plt.plot(c_data, func(c_data, popt[0]-perr[0],popt[1]+perr[1],popt[2]-perr[2],popt[3]+perr[3]), 'r--')
+
+        plt.xlabel('c')
+        plt.ylabel('fz (N)')
+        plt.title(r"a=1 m, H0=1 A/m, $\theta$=90, $\chi=$"+susc_strings[ind])
+        plt.grid('on')
+        plt.legend()
+        
+        if case_selection==0:
+            susc_strings=np.array(['0,10','0,15','0,20','0,25','0,50','0,75','1,0','1,5','2,0','2,5','3,0','3,5','4,0','4,5','5,0','5,5','6,0','6,5','7,0',\
+                '7,5','8,0','8,5','9,0','9,5','10,0','10,5','11,0','11,5','12,0','15,0','20,0','25,0','30,0','35,0','40,0','45,0','50,0','55,0','60,0','65,0','70,0','75,0','80,0','85,0','90,0','95,0','100,0'])
+            if func_selection==0:
+                plt.savefig('Plots/Fitting/Parallel_Case/a_1_H0_1_th_0_chi_'+susc_strings[ind]+'.png', bbox_inches='tight')
+            elif func_selection==1:
+                plt.savefig('Plots/Fitting/Parallel_Case/vary_a_1_H0_1_th_0_chi_'+susc_strings[ind]+'.png', bbox_inches='tight')
+        elif case_selection==1:
+            susc_strings=np.array(['0,10','0,15','0,20','0,25','0,50','0,75','1,0','1,5','2,0','2,5','3,0','3,5','4,0','4,5','5,0','5,5','6,0','6,5','7,0','7,5','8,0',\
+                '8,5','9,0','9,5','10,0','10,5','11,0','11,5','12,0','15,0','20,0','30,0','40,0','50,0','60,0','70,0','80,0','90,0','100,0'])
+            if func_selection==0:
+                plt.savefig('Plots/Fitting/Perpendicular_Case/a_1_H0_1_th_90_chi_'+susc_strings[ind]+'.png', bbox_inches='tight')
+            elif func_selection==1:
+                plt.savefig('Plots/Fitting/Perpendicular_Case/vary_a_1_H0_1_th_90_chi_'+susc_strings[ind]+'.png', bbox_inches='tight')
+    
+        plt.close()
