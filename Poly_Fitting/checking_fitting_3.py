60a2ef528df99aec84ab6666ce2c94fa1711a1a4
AstroAnmol <anmolsikka09@gmail.com>
2022-02-27 13:58:32 -0500

    parallel case from 2.2 complete func fitting

--- Poly_Fitting/checking_fitting_3.py
+++ Poly_Fitting/checking_fitting_3.py
@@ -4,46 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-susc_data=np.array([0.25,	0.5,	0.75,	1,	1.5,	2,	2.5,	3,	3.5,	4,	4.5,	5,	5.5,	6,	6.5,	7,	7.5,	8])
-c_data = np.linspace(2,4.2,12)
+susc_data=np.array([0.1,0.15,0.2,0.25,0.5,0.75,1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,\
+    7.5,8.0,8.5,9.0,9.5,10.0,10.5,11.0,11.5,12.0,15.0,20.0,25.0,30.0,35.0,40.0,45.0,50.0,55.0,60.0,65.0,70.0,75.0,80.0,85.0,90.0,95.0,100.0])
+c_data = np.linspace(2.2,4.2,11)
 
 susc_susc, c_c = np.meshgrid(susc_data,c_data)
 
-## Suscpetibility based
-
-# model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
-def func_og(data, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
-    c=data[0]
-    susc=data[1]
-    mu0=4.0*np.pi*1e-7
-    susc_eff=3*susc/(3+susc)
-    p1=a1*(susc**3) + b1*(susc**2) + c1*(susc) + d1
-    p2=a2*(susc**3) + b2*(susc**2) + c2*(susc) + d2
-    p3=a3*(susc**3) + b3*(susc**2) + c3*(susc) + d3
-    F_DM=4*np.pi*susc_eff**2/(3*c**4)
-    return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
-
-# model function F = muo*(po/r^4 + p1/r^5 + p2/r^6 + p3/r^7)
-def func_vary(data, a0, b0, c0, d0, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
-    c=data[0]
-    susc=data[1]
-    mu0=4.0*np.pi*1e-7
-    susc_eff=3*susc/(3+susc)
-    p0=a0*(susc**3) + b0*(susc**2) + c0*(susc) + d0
-    p1=a1*(susc**3) + b1*(susc**2) + c1*(susc) + d1
-    p2=a2*(susc**3) + b2*(susc**2) + c2*(susc) + d2
-    p3=a3*(susc**3) + b3*(susc**2) + c3*(susc) + d3
-    return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
-
-#Parameters
-    #original
-params=np.array([1.31198199, -82.14110991, 114.4557932, -39.79210857, -7.68819096, 449.9106508, -611.5820138, 210.3711939, \
-    11.49729053, -610.1528645, 786.9320166, -264.7552372]) 
-    #varied
-params_vary=np.array([-0.70279873, 79.72887768, -129.4843025, 46.08305654, 6.83399346, -695.6784868, 1054.342322, -378.1589243, \
-    -21.57297844, 1992.615126, -2974.872714, 1061.175141, 22.91004143, -1878.194582, 2729.463034, -964.0822822])
-
-# ## Effective Suscpetibility based
+# ## Suscpetibility based
 
 # # model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
 # def func_og(data, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
@@ -51,9 +18,9 @@ params_vary=np.array([-0.70279873, 79.72887768, -129.4843025, 46.08305654, 6.833
 #     susc=data[1]
 #     mu0=4.0*np.pi*1e-7
 #     susc_eff=3*susc/(3+susc)
-#     p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
-#     p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
-#     p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+#     p1=a1*(susc**3) + b1*(susc**2) + c1*(susc) + d1
+#     p2=a2*(susc**3) + b2*(susc**2) + c2*(susc) + d2
+#     p3=a3*(susc**3) + b3*(susc**2) + c3*(susc) + d3
 #     F_DM=4*np.pi*susc_eff**2/(3*c**4)
 #     return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
 
@@ -63,41 +30,78 @@ params_vary=np.array([-0.70279873, 79.72887768, -129.4843025, 46.08305654, 6.833
 #     susc=data[1]
 #     mu0=4.0*np.pi*1e-7
 #     susc_eff=3*susc/(3+susc)
-#     p0=a0*(susc_eff**3) + b0*(susc_eff**2) + c0*(susc_eff) + d0
-#     p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
-#     p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
-#     p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+#     p0=a0*(susc**3) + b0*(susc**2) + c0*(susc) + d0
+#     p1=a1*(susc**3) + b1*(susc**2) + c1*(susc) + d1
+#     p2=a2*(susc**3) + b2*(susc**2) + c2*(susc) + d2
+#     p3=a3*(susc**3) + b3*(susc**2) + c3*(susc) + d3
 #     return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
 
 # #Parameters
 #     #original
-# params=np.array([-1680.964425, 4341.955037, -3389.482164, 700.6027091, 9068.105659, -23362.69492, 18226.74997, -3766.233961, \
-#     -12010.89806, 30801.03636, -24012.0366, 4959.73752]) 
+# params=np.array([-0.03562863, 0.61172073, -2.70956668, 0.63888614, 0.16353389, -3.0435703, 16.17696363, -4.3861996, \
+#     0.16353389, -3.0435703, 16.17696363, -4.3861996]) 
 #     #varied
-# params_vary=np.array([1785.59961, -4677.810955, 3658.944886, -757.9334926, -15456.07901, 40364.57653, -31616.62959, 6547.725445, \
-#     43704.83787, -113939.502, 89202.28688, -18468.48752, -40480.91228, 105251.5642, -82351.10161, 17044.40468])
-#     #initial
+# params_vary=np.array([0.01786655, -0.47992324, 4.22817664, -1.26463416, -0.22357326, 3.54618869, -5.81412107, 1.82313041, \
+#     0.63610746, -10.4220993, 23.98314026, -7.36389373, -0.47999317, 8.78829318, -34.29359611, 11.49288475])
+
+## Effective Suscpetibility based
+
+# model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
+def func_og(data, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
+    c=data[0]
+    susc=data[1]
+    mu0=4.0*np.pi*1e-7
+    susc_eff=3*susc/(3+susc)
+    p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
+    p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
+    p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+    F_DM=4*np.pi*susc_eff**2/(3*c**4)
+    return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
+
+# model function F = muo*(po/r^4 + p1/r^5 + p2/r^6 + p3/r^7)
+def func_vary(data, a0, b0, c0, d0, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
+    c=data[0]
+    susc=data[1]
+    mu0=4.0*np.pi*1e-7
+    susc_eff=3*susc/(3+susc)
+    p0=a0*(susc_eff**3) + b0*(susc_eff**2) + c0*(susc_eff) + d0
+    p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
+    p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
+    p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+    return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
+
+#Parameters
+#     #original
 # params=np.array([2.1679403, -5.10571752, 0.91979498, -0.14520514, -11.99291281, 32.60814942, -8.5414691, 1.26892871, \
 #     12.57083862, -53.1809105, 17.62267505, -2.66077834]) 
+#     #varied
+# params_vary=np.array([-2.35006285, 8.70658619, -2.96749461, 0.51760021, 20.29772718, -39.95880704, 23.81296509, -4.13832093, \
+#     -57.57941163, 120.244874, -66.10561194, 11.3095235, 50.04124488, -125.2151948, 64.93845222, -10.91382394])
+    #initial
+params=np.array([-497.95224865,   2671.56697629,  -5718.94770409,   4061.8828376,
+   2944.52379528, -16054.0668261,   33639.65154115, -23897.55494789,
+  -4317.45216271,  23490.26719675, -48875.99378488,  34745.55102049]) 
 
 fz_og=func_og((c_c,susc_susc), *params)
-fz_vary=func_vary((c_c,susc_susc), *params_vary)
+# fz_vary=func_vary((c_c,susc_susc), *params_vary)
 
 # print(func_og(2,0.25))
 
-for ind in range(18):
-    susc_strings=np.array(['0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0','7.5','8.0'])
-    data_H1_a1_th00=pd.read_csv('Data/data_H1_a1_th0.csv')
+for ind in range(susc_data.size):
+    susc_strings=np.array(['0.1','0.15','0.2','0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0',\
+    '7.5','8.0','8.5','9.0','9.5','10.0','10.5','11.0','11.5','12.0','15.0','20.0','25.0','30.0','35.0','40.0','45.0','50.0','55.0','60.0','65.0','70.0','75.0','80.0','85.0','90.0','95.0','100.0'])
+    data_H1_a1_th00=pd.read_csv('Data/csv_files/data_H1_a1_th0.csv')
+    data_H1_a1_th00=data_H1_a1_th00.iloc[1:]
     fz_data=data_H1_a1_th00[susc_strings[ind]]
     fz_og_data=fz_og[:,ind]
-    fz_vary_data=fz_vary[:,ind]
+    # fz_vary_data=fz_vary[:,ind]
 
     #
     squaresum = np.sum((fz_data-np.mean(fz_data))**2)
     l_sq_err_og=np.sum(fz_og_data-fz_data)**2
     R2_og = 1 - (l_sq_err_og/squaresum)
-    l_sq_err_vary=np.sum(fz_vary_data-fz_data)**2
-    R2_vary = 1 - (l_sq_err_vary/squaresum)
+    # l_sq_err_vary=np.sum(fz_vary_data-fz_data)**2
+    # R2_vary = 1 - (l_sq_err_vary/squaresum)
     # print('Least squares error (FDM)',l_sq_err_og)
     # print('R^2 value (FDM)',R2_og)
     # print('Least squares error (vary)',l_sq_err_vary)
@@ -107,16 +111,18 @@ for ind in range(18):
     plt.figure()
     plt.plot(c_data,fz_data,'b+',label="multipole data")
     plt.plot(c_data,fz_og_data,'r-',label=("fitting with F_DM ($R^2$ %% =%5.3f)" %(R2_og*100)))
-    plt.plot(c_data,fz_vary_data,'g--',label=("fitting with p0 ($R^2$ %% =%5.3f)" %(R2_vary*100)))
+    # plt.plot(c_data,fz_vary_data,'g--',label=("fitting with p0 ($R^2$ %% =%5.3f)" %(R2_vary*100)))
     plt.xlabel('c')
     plt.ylabel('fz (N)')
-    plt.title(r"a=1 m, H0=1 A/m, $\theta$=0, $\chi=$"+susc_strings[ind])
+    # plt.title(r"a=1 m, H0=1 A/m, $\theta$=0, $\chi=$"+susc_strings[ind])
     # plt.title(r"a=1 m, H0=1 A/m, $\theta$=0, $\chi=$"+susc_strings[ind]+ "(based on $\chi_{eff}$)")
-    # plt.title("a=1 m, H0=1 A/m, $\theta$=0, $\chi=$"+susc_strings[ind]+ "(based on $\chi_{eff}$ and initial guess)")
+    plt.title(r"a=1 m, H0=1 A/m, $\theta$=0, $\chi=$"+susc_strings[ind]+ "(based on $\chi_{eff}$ and initial guess)")
     plt.grid('on')
     plt.legend()
     # plt.show()
-    susc_strings=np.array(['0,25','0,50','0,75','1,0','1,5','2,0','2,5','3,0','3,5','4,0','4,5','5,0','5,5','6,0','6,5','7,0','7,5','8,0'])
-    plt.savefig('Plots/Fitting/Parallel_Case/Cubic_Poly/Complete_Model_Function/Susceptibility_Based/a_1_H0_1_th_0_chi_'+susc_strings[ind]+'.png')
-    # plt.savefig('Plots/Fitting/Parallel_Case/Cubic_Poly/Complete_Model_Function/Effective_Susceptibility_Based/a_1_H0_1_th_0_chi_'+susc_strings[ind]+'_chi_eff'+'.png')
-    # plt.savefig('Plots/Fitting/Parallel_Case/Cubic_Poly/Complete_Model_Function/Effective_Susceptibility_Initial/a_1_H0_1_th_0_chi_'+susc_strings[ind]+'_chi_eff'+'.png')
+    susc_strings=np.array(['0.1','0.15','0.2','0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0',\
+    '7.5','8.0','8.5','9.0','9.5','10.0','10.5','11.0','11.5','12.0','15.0','20.0','25.0','30.0','35.0','40.0','45.0','50.0','55.0','60.0','65.0','70.0','75.0','80.0','85.0','90.0','95.0','100.0'])
+    # plt.savefig('Plots/Fitting/Parallel_Case/Complete_Model_Function/Susceptibility_Based/a_1_H0_1_th_00_chi_'+susc_strings[ind]+'.png')
+    # plt.savefig('Plots/Fitting/Parallel_Case/Complete_Model_Function/Effective_Susceptibility_Based/a_1_H0_1_th_00_chi_'+susc_strings[ind]+'_chi_eff'+'.png')
+    plt.savefig('Plots/Fitting/Parallel_Case/Cubic_Poly/Complete_Model_Function/Effective_Susceptibility_Initial/a_1_H0_1_th_00_chi_'+susc_strings[ind]+'_chi_eff'+'.png', bbox_inches='tight')
+    plt.close()
--- Poly_Fitting/complete_function_fit.py
+++ Poly_Fitting/complete_function_fit.py
@@ -1,46 +1,72 @@
-## Parallel Case
-
+## Parallel case
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 import pandas as pd
+import sys
 
+file_path = 'Results_txt/complete_fitting_results_th00.txt'
+sys.stdout = open(file_path, "w")
 
 #import the data
-data_H1_a1_th0=pd.read_csv('Data/data_H1_a1_th0.csv')
+data_H1_a1_th00=pd.read_csv('Data/csv_files/data_H1_a1_th0.csv', index_col=0, header=None)
+
+start_susc=19
+end_susc=43
+
+data=data_H1_a1_th00.drop([20,21,22,23,24,26,27,28,29], axis=1)
+data=data.drop(['2.0'],axis=0)
+
 #create mesh arrays
-c_data=data_H1_a1_th0['c']
-susc_data=np.array([0.25,	0.5,	0.75,	1,	1.5,	2,	2.5,	3,	3.5,	4,	4.5,	5,	5.5,	6,	6.5,	7,	7.5,	8])
+c_data = np.linspace(2.2,4.2,11)
+susc_data=np.array(data.loc['c', start_susc:end_susc])
 c_c, susc_susc = np.meshgrid(c_data, susc_data)
-susc_susc=np.reshape(susc_susc,18*12)
-c_c=np.reshape(c_c,18*12)
-susc_strings=np.array(['0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0','7.5','8.0'])
-fz_data=data_H1_a1_th0['0.25']
-for ind in range(17):
-    fz_data=np.append(fz_data,data_H1_a1_th0[susc_strings[ind+1]])
+susc_susc=np.reshape(susc_susc,16*11)
+c_c=np.reshape(c_c,16*11)
+data.columns=data.iloc[0]
+data=data[1:]
+# susc_strings=np.array(['0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0','6.5','7.0','7.5','8.0'])
+fz_data=data[susc_data[0]]
+for ind in range(15):
+    fz_data=np.append(fz_data,data[susc_data[ind+1]])
 
+# print(data)
+# print(fz_data)
 
-
-## Polynomial fitting for mag data
+# Effective Suscpetibility based
 
 # model function F = muo*(Fdm + p1/r^5 + p2/r^6 + p3/r^7)
-def func(data, a0, b0, c0, a1, b1, c1):
+def func(data, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
     c=data[0]
     susc=data[1]
     mu0=4.0*np.pi*1e-7
     susc_eff=3*susc/(3+susc)
-    p1=a0*(susc_eff**a1)
-    p2=b0*(susc_eff**b1)
-    p3=c0*(susc_eff**c1)
-    F_DM=-8*np.pi*susc_eff**2/(3*c**4)
+    p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
+    p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
+    p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+    F_DM=4*np.pi*susc_eff**2/(3*c**4)
     return mu0*(F_DM + p1/(c**5) + p2/(c**6) + p3/(c**7))
 
-# #model function F = mu0*(p0/r^ + p1/r^5 + p2/r^6 + p3/r^7)
-# def func(c, p0, p1, p2, p3):
-#     mu0=4.0*np.pi*1e-7
-#     return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
+# model function F = muo*(po/r^4 + p1/r^5 + p2/r^6 + p3/r^7)
+def func_vary(data, a0, b0, c0, d0, a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3):
+    c=data[0]
+    susc=data[1]
+    mu0=4.0*np.pi*1e-7
+    susc_eff=3*susc/(3+susc)
+    p0=a0*(susc_eff**3) + b0*(susc_eff**2) + c0*(susc_eff) + d0
+    p1=a1*(susc_eff**3) + b1*(susc_eff**2) + c1*(susc_eff) + d1
+    p2=a2*(susc_eff**3) + b2*(susc_eff**2) + c2*(susc_eff) + d2
+    p3=a3*(susc_eff**3) + b3*(susc_eff**2) + c3*(susc_eff) + d3
+    return mu0*(p0/(c**4) + p1/(c**5) + p2/(c**6) + p3/(c**7))
 
-# plt.plot(c_data, fz_data, 'b+', label='data')
+#Parameters
+    #original
+params=np.array([ -497.95224865,  2671.56697629, -5718.94770409,  4061.8828376, 2944.52379528, -16054.0668261,   33639.65154115, -23897.55494789,\
+    -4317.45216271,  23490.26719675, -48875.99378488,  34745.55102049]) 
+    #varied
+params_vary=np.array([528.15468697, -3044.27405624,  6334.4222927,  -4545.11737575, -4898.61623327,  28071.66585806, -58497.71993539,  41931.88594689, \
+    14937.00471449,  -85273.02550456,  177469.1475836,  -127098.37913982, -15030.02324746,   85321.87679201, -177355.5758031,   126932.593797])
+
 
 #Fit for the parameters of the function func:
 print('Fit using complete model function')
@@ -62,7 +88,6 @@ print('R-squared:', Rsquared, '\n')
 
 #Check earlier model fit
 print('Fit using parameters separately')
-params=np.array([-24.754,	140.732, 	-205.165,    6.394,	    6.337,  	6.233])
 print('fitted parameters', params)
 
 modelPredictions = func((c_c,susc_susc), *params) 
@@ -141,21 +166,6 @@ Rsquared = 1.0 - (np.var(absError) / np.var(fz_data))
 print('RMSE:', RMSE)
 print('R-squared:', Rsquared, '\n')
 
-#Check Stability of the fitted parameters
-print('Check stability of fitted parameters (5% less)')
-params=popt - popt*0.05
-print('Parameters', params)
-
-modelPredictions = func((c_c,susc_susc), *params) 
-
-absError = modelPredictions - fz_data
-
-SE = np.square(absError) # squared errors
-MSE = np.mean(SE) # mean squared errors
-RMSE = np.sqrt(MSE) # Root Mean Squared Error, RMSE
-Rsquared = 1.0 - (np.var(absError) / np.var(fz_data))
-print('RMSE:', RMSE)
-print('R-squared:', Rsquared, '\n') 
 
 #Check Stability of the fitted parameters
 print('Check stability of fitted parameters (5% less)')
@@ -177,7 +187,7 @@ print('R-squared:', Rsquared, '\n')
 for i in range(100):
     # Generate random parameters around the popt parameters
     print('Check stability around the fitted parameters (5%)', i+1, 'th run')
-    rand_set=np.random.uniform(-0.05,0.05,6)
+    rand_set=np.random.uniform(-0.05,0.05, 12)
     print('Random set', rand_set)
     params=popt+ np.multiply(popt,rand_set)
     print('Parameters', params)
